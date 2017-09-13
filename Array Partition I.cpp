718e5efcf50ebcf9775073091a1aad041227f288
Zach-Leo <30796614+Zach-Leo@users.noreply.github.com>
2017-09-13 14:41:35 +0800

    Update Array Partition I.cpp

--- Array Partition I.cpp
+++ Array Partition I.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        vector<int> (20001,0);
+        vector<int> hashtable(20001,0);
         for(size_t i=0;i<nums.size();i++)
         {
             hashtable[nums[i]+10000]++;
